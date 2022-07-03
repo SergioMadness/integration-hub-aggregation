13407b94f351814881e067715620d692376e3b15
Сергей Зинченко <you@example.com>
2022-07-03 17:57:50 +0300

    Register subsystem in pool

--- src/Providers/IntegrationHubAggregationProvider.php
+++ src/Providers/IntegrationHubAggregationProvider.php
@@ -1,7 +1,9 @@
 <?php namespace professionalweb\IntegrationHub\IntegrationHubAggregation\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use professionalweb\IntegrationHub\IntegrationHubAggregation\Models\AggregationOptions;
 use professionalweb\IntegrationHub\IntegrationHubAggregation\Interfaces\GetItemSubsystem;
+use professionalweb\IntegrationHub\IntegrationHubCommon\Interfaces\Services\SubsystemPool;
 use professionalweb\IntegrationHub\IntegrationHubAggregation\Services\AggregationSubsystem;
 use professionalweb\IntegrationHub\IntegrationHubAggregation\Repositories\AggregationRepository;
 use professionalweb\IntegrationHub\IntegrationHubAggregation\Services\GetAggregatedItemSubsystem;
@@ -13,6 +15,13 @@ class IntegrationHubAggregationProvider extends ServiceProvider
     public function boot(): void
     {
         $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+        $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'IntegrationHubAggregation');
+
+        $this->app->booted(static function () {
+            /** @var SubsystemPool $pool */
+            $pool = app(SubsystemPool::class);
+            $pool->register(trans('IntegrationHubAggregation::common.name'), AggregationSubsystem::AGGREGATION_SUBSYSTEM, new AggregationOptions());
+        });
     }
 
     public function register(): void
